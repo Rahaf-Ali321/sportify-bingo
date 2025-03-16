42aa6d2cb0b43a01bd650305ecc6d069e63d0ff2
Rahaf-Ali321 <alirahaf275@gmail.com>
2025-03-16 05:15:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function translateText() {
+    let inputText = document.getElementById("inputText").value;
+    document.getElementById("outputText").innerText = "النتيجة: " + inputText; // Placeholder
+}
